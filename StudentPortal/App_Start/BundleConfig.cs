5eaaba894911c94a89b952b66b541dac521ec579
manish <manishgupta.98@hotmail.com>
2024-10-12 17:43:23 +0530

    new manish studentprotal commited

---
+++ StudentPortal/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace StudentPortal
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                    "~/Content/js/bootstrap.bootstrap.min.js",
+                     "~/Content/js/bootstrap.bootstrap.js"
+                     )); 
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/bootstrap.min.css"
+                      )); 
+        }
+    }
+}
